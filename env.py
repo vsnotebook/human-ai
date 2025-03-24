74567baaee8ee5ab86b1a110734a3340cbb0ca31
vsfrank <vsfrank@qq.com>
2025-03-24 14:15:45 +0800

    中文

--- env.py
+++ env.py
@@ -6,8 +6,11 @@ PROJECT_ID = "human-ai-454609"
 CREDENTIALS_PATH = "human-ai-454609-bf84b910d612.json"
 
 # Proxy settings
-os.environ["http_proxy"] = "http://127.0.0.1:7897"
-os.environ["https_proxy"] = "http://127.0.0.1:7897"
+# os.environ["http_proxy"] = "http://127.0.0.1:7897"
+# os.environ["https_proxy"] = "http://127.0.0.1:7897"
+
+os.environ["http_proxy"] = "http://127.0.0.1:10808"
+os.environ["https_proxy"] = "http://127.0.0.1:10808"
 
 # Server settings
 SERVER_HOST = "0.0.0.0"
