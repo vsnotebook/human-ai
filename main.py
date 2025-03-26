d939a1de989459984a39c94b09b3323ee29fd63c
vsfrank <vsfrank@qq.com>
2025-03-26 15:15:07 +0800

    支付修改

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware  # 添加这行
 from starlette.middleware.sessions import SessionMiddleware
@@ -10,10 +10,19 @@ from env import SERVER_HOST, SERVER_PORT
 
 app = FastAPI()
 
+
+def check_allowed_origin(origin: str, request: Request) -> bool:
+    # 仅当请求路径为 /transcribe 且来源为 http://127.0.0.1:8080 时允许
+    # return request.url.path == "/transcribe" and origin == "http://127.0.0.1:8080"
+    pass_flag = request.url.path == "/transcribe"
+    return pass_flag
+
+
 # 添加 CORS 中间件
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # 允许所有源，生产环境建议设置具体的域名
+    # allow_origins=check_allowed_origin,  # 动态验证源
     allow_credentials=True,
     allow_methods=["*"],  # 允许所有方法
     allow_headers=["*"],  # 允许所有请求头
